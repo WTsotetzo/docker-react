c10072bd9f83c61fdac13dab88e609274ce86ac8
Tsotetzo Willy <w.tsotetzo@b-a-w.com>
2021-05-07 12:41:24 +0200

    weeeeeeesh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          my nigga my nigga
         </a>
       </header>
     </div>
